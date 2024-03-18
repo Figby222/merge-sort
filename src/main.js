c9152d517714fab683f8939214d4bfcd7419748c
Figby222 <figby222@outlook.com>
2024-03-18 02:58:50 -0700

    Fix while condition in sortVals()

--- src/main.js
+++ src/main.js
@@ -4,13 +4,20 @@ function sortVals(arr1, arr2) {
     let k = 0;
     let result = [];
 
-    while(i < arr1.length && j < arr2.length) {
+    while(i < arr1.length || j < arr2.length) {
         if (arr1[i] < arr2[j] || !(arr2[j])) { // if i of arr1 is smaller than j of arr2 or j of arr2 doesn't exist
             result[k] = arr1[i]; // assign k of result to j of arr2
             i++;
             k++;
         } else if (arr1[i] > arr2[j] || !(arr1[i])) { // if i of arr1 is bigger than j of arr2 or i of arr1 doesnt exist
             result[k] = arr2[j]; // assign k of result to j of arr2
+            j++;
+            k++;
+        } else { // arr1[i] == arr2[j]
+            result[k] = arr1[i];
+            i++;
+            k++;
+            result[k] = arr2[j];
             j++;
             k++;
         }
@@ -22,11 +29,11 @@ function sortVals(arr1, arr2) {
 function mergeSort(arr) {
     let arrI;
     let arrJ;
-    if (arr.length == 1) {
+    if (arr.length <= 1) {
         return arr;
     } else {
-        arrI = mergeSort(arr.slice(0, (arr.length / 2) - 1)) // merge sort left side of arr
-        arrJ = mergeSort(arr.slice((arr.length / 2) - 1)); // merge sort right side of arr
+        arrI = mergeSort(arr.slice(0, (arr.length / 2))) // merge sort left side of arr
+        arrJ = mergeSort(arr.slice((arr.length / 2))); // merge sort right side of arr
     }
 
     
